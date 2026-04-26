9fa633b9309b25af1f92cbbde832fd3ca666a3f6
Lauren Meisner <lauren@centennialworld.com>
2026-04-26 15:10:23 +1000

    Reset Supabase client to simple version, remove middleware

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,11 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   "https://vpdhlsnakvnxrfglphmn.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwZGhsc25ha3ZueHJmZ2xwaG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMzkyOTIsImV4cCI6MjA5MTgxNTI5Mn0.VVUu7eM6AKqtgWasEU7yLtmS2tSe-d5rUqTmWHLBaC8",
-  {
-    auth: {
-      persistSession: true,
-      storage: typeof window !== "undefined" ? window.localStorage : undefined,
-    }
-  }
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwZGhsc25ha3ZueHJmZ2xwaG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMzkyOTIsImV4cCI6MjA5MTgxNTI5Mn0.VVUu7eM6AKqtgWasEU7yLtmS2tSe-d5rUqTmWHLBaC8"
 )
